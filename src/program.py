41e9cdd5b6e2bf5fbd35206492abbe681af6325e
Amir Sharapov <amir@amirfounder.com>
2021-12-12 00:32:22 -0600

    fully refactored program to functional programming'

--- src/program.py
+++ src/program.py
@@ -1,71 +1,49 @@
 import os
-
-from multiprocessing import Process, Value
-from src import Bot, BotBuilder, DatabaseManager, \
-  DataManager, InteractionAgent, Mouse
-from src.builders.script_builder.src.JavascriptBuilder import JavascriptBuilder
-from src.interaction_agent.src.agents.InstagramAgent import InstagramAgent
-from src.console.src import Console \
-
-def run() -> Value:
-  
-  os.system("mitmdump -s src/http_listener/listener.py --set console_eventlog_verbosity=error termlog_verbosity=error")
+from multiprocessing import Process
 
 
+def run():
 
+    processes: list[Process] = []
+    drivers: list[function] = [
+        run_data_manager,
+        run_http_listener,
+        run_interaction_logger,
+        run_bot_builder,
+        run_content_builder,
+        run_instagram_agent
+    ]
 
-class ApplicationRunner():
-
-  @staticmethod
-  def run():
-    ApplicationRunner.run_multiple([
-      # ApplicationRunner.run_http_listener,
-      ApplicationRunner.run_sandbox
-      # ApplicationRunner.run_content_builder
-    ])
-  
-  @staticmethod
-  def run_data_manager():
-    data_manager = DataManager()
-    data_manager.run()
-    pass
-
-  @staticmethod
-  def run_http_listener():
-    os.system("mitmdump -s src/http_listener/src/app.py --set console_eventlog_verbosity=error termlog_verbosity=error")
-  
-  @staticmethod
-  def run_interaction_logger():
-    pass
- 
-  @staticmethod
-  def run_bot_builder():
-    bot_builder = BotBuilder()
-    bot_builder.build_bots()
-
-  @staticmethod
-  def run_content_builder():
-    os.system('npm start')
-
-  @staticmethod
-  def run_interaction_proxy():
-    interaction_proxy = InteractionAgent()
-    interaction_proxy.start()
-  
-  @staticmethod
-  def run_sandbox():
-    prompt = Console()
-
-  @staticmethod
-  def run_multiple(processes):
-    runners = []
+    for driver in drivers:
+        process = Process(target=driver)
+        processes.append(process)
 
     for process in processes:
-      runner = Process(target=process)
-      runners.append(runner)
-    
-    for runner in runners:
-      runner.start()
-    
-    for runner in runners:
-      runner.join()
+        process.start()
+
+    for process in processes:
+        process.join()
+
+
+def run_data_manager():
+    pass
+
+
+def run_http_listener():
+    os.system("mitmdump -s src/http_listener/listener.py --set console_eventlog_verbosity=error termlog_verbosity=error")
+
+
+def run_interaction_logger():
+    pass
+
+
+def run_bot_builder():
+    pass
+
+
+def run_content_builder():
+    os.system('npm start')
+
+
+def run_instagram_agent():
+    pass
