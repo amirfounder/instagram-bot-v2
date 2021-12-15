5b2a242fc1573e43eaca02ca85304d06bb07ec2f
Amir Sharapov <amir@amirfounder.com>
2021-12-14 23:07:14 -0600

    updated python to version 3.10. had to play with requirements.txt a bit

--- src/program.py
+++ src/program.py
@@ -1,6 +1,7 @@
 import os
 from multiprocessing import Process, Manager
-from typing import Any
+from multiprocessing.process import AuthenticationString
+from typing import Any, Callable, Mapping, Tuple
 
 from src.data_manager.database import setup as setup_database
 from src.console import run_client as run_console_client, run_server as run_console_server
@@ -66,13 +67,43 @@ def run_multiple_processes(drivers: list):
 
     for process in processes:
         process.start()
-    
-    return processes
 
+    return processes
 
 
 def spawn_process(target, args: tuple[Any] = ()):
     process = Process(target=target, args=args)
     process.start()
-    
+
     return process
+
+
+class CustomProcess(Process):
+    def __init__(
+            self,
+            group: None = ...,
+            target: Callable[..., Any] | None = ...,
+            name: str | None = ...,
+            args: Tuple[Any, ...] = ...,
+            kwargs: Mapping[str, Any] = ..., *,
+            daemon: bool | None = ...) -> None:
+        super().__init__(
+            group=group,
+            target=target,
+            name=name,
+            args=args,
+            kwargs=kwargs,
+            daemon=daemon)
+
+    def __getstate__(self):
+        state = self.__dict__.copy()
+        conf = state['_config']
+        if 'authkey' in conf:
+            conf['authkey'] = bytes(conf['authkey'])
+        return state
+    
+    def __setstate__(self, state):
+        state['_config']['authkey'] = AuthenticationString(
+            state['_config']['authkey']
+        )
+        self.__dict__.update(state)
