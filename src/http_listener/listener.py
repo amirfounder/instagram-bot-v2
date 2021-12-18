9415a4737bea44ac595f6cb3a67b82c83fb95420
Amir Sharapov <amir@amirfounder.com>
2021-12-18 13:51:10 -0600

    updated listener from old code

--- src/http_listener/listener.py
+++ src/http_listener/listener.py
@@ -1,10 +1,7 @@
-import importlib
 import sys
-import time
 sys.path.append('C:/Users/Amir Sharapov/Code/amirfounder-automation/x')
 
 from src.http_listener.handle_flow import handle_flow
-import src.config as config
 
 
 def request(flow) -> None:
